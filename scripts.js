0705f8339329cbbf457f31b304f4df4891748c07
Doaa-Khaled650 <doaakhaled7965@gmail.com>
2024-07-26 02:26:37 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+function storeName() {
+    const userName = document.getElementById('nameInput').value;
+    localStorage.setItem('Doaa Khaled','amira mohamed','hanan amir', userName);
+ console.log('Name stored:', userName);
+}
